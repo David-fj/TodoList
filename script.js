fa7ea4202a2c4108b100152e746ec31e4fa05a58
David-fj <soprasvr@gmail.com>
2024-08-12 18:31:48 -0300

    asdsa

---
+++ script.js
@@ -0,0 +1,19 @@
+const inputAdd = document.querySelector(".inputTodoList");
+const buttonInput = document.querySelector(".buttonInput")
+const section = document.querySelector(".atividadesContainer")
+let list = []
+buttonInput.addEventListener('click', () => {
+    section.innerHTML += `
+    <div class="atividadeContainer">
+        <input type="checkbox">
+        <p>${String(inputAdd.value)}</p>
+        <button onclick="edit(${list.length})" class="buttonEdit"><img src="imagens/edit_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg"></button>
+        <button onclick="" class="buttonTrash"><img src="imagens/delete_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg"></button>
+    </div>` 
+    list = document.querySelectorAll(".atividadeContainer")
+})
+
+function edit(i) {
+    inputAdd.value = String(list[i].children[1].innerHTML)
+    console.log(list[i].children[1])
+}

928e9ffd1848db9b472e80bac1f2dd41e51f10d6
Davif-fj <soprasvr@gmail.com>
2024-08-14 15:52:21 -0300

    asdasdas

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let list = []
 buttonInput.addEventListener('click', () => {
     section.innerHTML += `
     <div class="atividadeContainer">
-        <input type="checkbox">
+        <input type="checkbox" onchange="check(${list.length})">
         <p>${String(inputAdd.value)}</p>
         <button onclick="edit(${list.length})" class="buttonEdit"><img src="imagens/edit_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg"></button>
         <button onclick="" class="buttonTrash"><img src="imagens/delete_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg"></button>
@@ -13,7 +13,11 @@ buttonInput.addEventListener('click', () => {
     list = document.querySelectorAll(".atividadeContainer")
 })
 
+function check(i) {
+    list[i].classList.toggle("checkStyle")
+    console.log("foi");
+}
+
 function edit(i) {
     inputAdd.value = String(list[i].children[1].innerHTML)
-    console.log(list[i].children[1])
 }
